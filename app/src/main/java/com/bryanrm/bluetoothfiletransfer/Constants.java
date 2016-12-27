e796bd4cdab606cf6f6222d3597243ecb2b081ef
Bryan <martineb1@hawkmail.newpaltz.edu>
2016-12-27 18:49:40 -0500

    Initial commit

---
+++ app/src/main/java/com/bryanrm/bluetoothfiletransfer/Constants.java
@@ -0,0 +1,12 @@
+package com.bryanrm.bluetoothfiletransfer;
+
+/**
+ * Created by Bryan R Martinez on 12/27/2016.
+ */
+
+public class Constants {
+    public static final int REQUEST_ENABLE_BT = 0;
+    public static final int REQUEST_READ_CODE = 1;
+
+    public static final String UUID = "00001105-0000-1000-8000-00805F9B34FB";
+}
---
+++ app/src/main/java/com/bryanrm/bluetoothfiletransfer/MainActivity.java
@@ -0,0 +1,73 @@
+package com.bryanrm.bluetoothfiletransfer;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (BluetoothAdapter.getDefaultAdapter() == null) { showNoBluetoothSupportDialog(); }
+    }
+
+    public void enableBluetooth(View view) {
+        if (!BluetoothAdapter.getDefaultAdapter().isEnabled()) {
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetoothIntent, Constants.REQUEST_ENABLE_BT);
+        }
+        else { createToast(getString(R.string.toast_bt_enabled)); }
+    }
+
+    public void selectFile(View view) {
+        Intent fileChooser = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        fileChooser.addCategory(Intent.CATEGORY_OPENABLE);
+        fileChooser.setType("*/*");
+        startActivityForResult(fileChooser, Constants.REQUEST_READ_CODE);
+    }
+
+    public void sendFile(View view) {
+        Intent sendFileIntent = new Intent(this, SendActivity.class);
+        if (uri != null) {
+            sendFileIntent.putExtra("URI", uri);
+            startActivity(sendFileIntent);
+        } else { createToast(getString(R.string.toast_error_select_file)); }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Constants.REQUEST_ENABLE_BT && resultCode == Activity.RESULT_OK)
+            createToast(getString(R.string.toast_bt_enabled));
+        else { createToast(getString(R.string.toast_error_enable_bt)); }
+        if (requestCode == Constants.REQUEST_READ_CODE && resultCode == Activity.RESULT_OK)
+            if (data != null) { uri = data.getData(); }
+    }
+
+    private void createToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    private void showNoBluetoothSupportDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(getResources().getString(R.string.dialog_error_title))
+                .setMessage(getResources().getString(R.string.dialog_error_message))
+                .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) { System.exit(0); }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setCancelable(false)
+                .show();
+    }
+}

5130eec7245288972c05b6ced2a0f0199429ddfc
Bryan <martineb1@hawkmail.newpaltz.edu>
2016-12-27 21:13:55 -0500

    Added list view

--- app/src/main/java/com/bryanrm/bluetoothfiletransfer/BTDeviceDiscover.java
+++ app/src/main/java/com/bryanrm/bluetoothfiletransfer/BTDeviceDiscover.java
@@ -1,8 +1,6 @@
 package com.bryanrm.bluetoothfiletransfer;
 
-import android.os.AsyncTask;
-
 // Created by Bryan R. Martinez on 12/27/2016
-public class BTDeviceDiscover extends AsyncTask {
+//public class BTDeviceDiscover extends AsyncTask {
 
-}
+//}
--- app/src/main/java/com/bryanrm/bluetoothfiletransfer/SendActivity.java
+++ app/src/main/java/com/bryanrm/bluetoothfiletransfer/SendActivity.java
@@ -1,12 +1,19 @@
 package com.bryanrm.bluetoothfiletransfer;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 // Created by Bryan R. Martinez on 12/27/2016
 public class SendActivity extends AppCompatActivity {
     private Uri uri;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,22 @@ public class SendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_send);
 
         uri = getIntent().getData();
+        arrayList = new ArrayList<>();
+        populateListView();
+    }
+
+    private void populateListView() {
+        arrayList.clear();
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        for (BluetoothDevice device : bluetoothAdapter.getBondedDevices()) {
+            arrayList.add(device.getName() + System.lineSeparator() + device.getAddress());
+            System.out.println(device.getName() + System.lineSeparator() + device.getAddress());
+        }
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter
+                = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
     }
 
 }
